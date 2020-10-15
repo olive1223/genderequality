ae56076bcaf68069e2809a4cdd29641bed2c9bb2
Olivia Huang <30355277+olive1223@users.noreply.github.com>
2020-10-14 23:47:10 -0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,48 @@
+function us() {
+ var usa = document.getElementById("usa");
+ usa.classList.toggle("show");}
+function mex() {
+  var mx = document.getElementById("mexico");
+  mx.classList.toggle("show");}
+function guat() {
+  var gm = document.getElementById("guatemala");
+  gm.classList.toggle("show");}
+function pru() {
+  var pe = document.getElementById("peru");
+  pe.classList.toggle("show");}
+function brz() {
+  var br = document.getElementById("brazil");
+  br.classList.toggle("show");}
+function arg() {
+  var ar = document.getElementById("argentina");
+  ar.classList.toggle("show");}
+function irl() {
+  var ir = document.getElementById("ireland");
+  ir.classList.toggle("show");}
+function est() {
+  var es = document.getElementById("estonia");
+  es.classList.toggle("show");}
+function vat() {
+  var city = document.getElementById("vatican");
+  city.classList.toggle("show");}
+function mli() {
+  var mal = document.getElementById("mali");
+  mal.classList.toggle("show");}
+function con() {
+ var drc = document.getElementById("congo");
+  drc.classList.toggle("show");}
+function som() {
+  var sma = document.getElementById("somalia");
+  sma.classList.toggle("show");}
+function sau() {
+  var ara = document.getElementById("saudi");
+  ara.classList.toggle("show");}
+function ind() {
+  var ina = document.getElementById("india");
+  ina.classList.toggle("show");}
+function chn() {
+  var cha = document.getElementById("china");
+  cha.classList.toggle("show");}
+function aus() {
+  var ast = document.getElementById("australia");
+  ast.classList.toggle("show");}
